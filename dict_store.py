0e4c38a44b7df39fd44197f5279bad4ae3b0af5b
Sebastian Aguiar <sebastianaguiar9511@gmail.com>
2026-03-26 14:35:52 -0500

    store2

--- dict_store.py
+++ dict_store.py
@@ -1,8 +1,8 @@
 # Write your code here!
 
 def temp_and_color(dict):
-    dict.get("temp", "None"
-             "color", "None")
+    dict.get("temp", "None")
+    dict.get("color", "None")
     return (temp_and_color)
 
 dict = {}
