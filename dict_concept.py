bec9827a9827e6cfe32e762ebef399dd27cd3aa0
Sebastian Aguiar <sebastianaguiar9511@gmail.com>
2026-03-26 14:34:22 -0500

    dict store

--- dict_concept.py
+++ dict_concept.py
@@ -1,1 +1,5 @@
 # Write your code here!
+
+dict = {"width": 480, "height": 480, "color_mode": "dark", "sensitivity": 0.4}
+def get_config():
+    return dict
--- dict_store.py
+++ dict_store.py
@@ -1,1 +1,8 @@
 # Write your code here!
+
+def temp_and_color(dict):
+    dict.get("temp", "None"
+             "color", "None")
+    return (temp_and_color)
+
+dict = {}
